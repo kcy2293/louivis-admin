0634d184f4e6758a4dff31641c54d2cd0420eb39
kcy2293 <kcy2293.nate.com>
2016-02-09 15:04:59 +0900

    기본헤더 및 로그인 페이지
    부트스트랩 샘플페이지를 이용한 기초화면 구성

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('login', {title: '로그인'});
 });
 
 module.exports = router;
